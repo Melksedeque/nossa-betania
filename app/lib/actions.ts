90211fe83309f3f362653d8e3e4cc1c80a2aa98e
Melksedeque <freelancer@melksedeque.com.br>
2026-01-14 16:48:47 -0300

    Correção de chamada Zod para e-mail com z.email()

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -9,7 +9,7 @@ import { revalidatePath } from 'next/cache';
 
 const RegisterSchema = z.object({
   name: z.string().min(2, { message: 'Nome deve ter pelo menos 2 caracteres.' }),
-  email: z.string().email({ message: 'Email inválido.' }),
+  email: z.email({ message: 'Email inválido.' }),
   password: z.string().min(6, { message: 'A senha deve ter pelo menos 6 caracteres.' }),
 });
 
