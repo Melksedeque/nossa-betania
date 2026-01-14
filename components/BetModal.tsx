d62caf8e458b8207ee56b36f8c62c1574d60fcde
Melksedeque <freelancer@melksedeque.com.br>
2026-01-13 23:23:54 -0300

    Correção de estado travado

--- components/BetModal.tsx
+++ components/BetModal.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Button } from '@/components/Button';
 import { Card } from '@/components/Card';
 import { placeBet } from '@/app/lib/actions';
