4a3600357045a7b9111a7b081df23ae1dbdd9a7b
Melksedeque <freelancer@melksedeque.com.br>
2026-01-16 15:34:11 -0300

    Espaçamento do spacer

--- components/SectionDivider.tsx
+++ components/SectionDivider.tsx
@@ -1,5 +1,5 @@
-export function SectionDivider() {
+export function SectionDivider({ space = 'my-0' }: { space?: string }) {
   return (
-    <div className="w-full h-px bg-linear-to-r from-transparent via-orange-500/50 to-transparent my-12" />
+    <div className={`w-full h-px bg-linear-to-r from-transparent via-orange-500/50 to-transparent ${space}`} />
   );
 }
